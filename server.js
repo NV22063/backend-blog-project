d681d71e14b3e94b3e1fa2b79d00114762ba41c0
NV22063 <155245438+NV22063@users.noreply.github.com>
2024-03-25 20:16:28 +0000

    15- custom error handler NV22063

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import {
+  errorResponserHandler,
+  invalidPathHandler,
+} from "./middleware/errorHandler";
 
 // Routes
 import userRoutes from "./routes/userRoutes";
@@ -16,6 +20,9 @@ app.get("/", (req, res) => {
 
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.use(invalidPathHandler);
+app.use(errorResponserHandler);
+
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
